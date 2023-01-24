4753b8573f944bf5f9243e3cfc2c08b14a19f0df
Deepakdev-lab <81613190+Deepakdev-lab@users.noreply.github.com>
2023-01-24 12:47:10 +0530

    Add files via upload
    Encryption and Decryption using AES_GCM Cryptography

---
+++ Encrypt_Decrypt_With_AES_GCM.py
@@ -0,0 +1,41 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import os
+
+# Generate a random 256-bit key (32 bytes)
+key = os.urandom(32)
+
+# Generate a random 12-byte IV
+iv = os.urandom(12)
+
+# Create a new AES-GCM cipher
+cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
+encryptor = cipher.encryptor()
+
+# Encrypt the message
+actual_message = "This is a secret message."
+message = bytes(actual_message, 'utf-8')
+
+ciphertext = encryptor.update(message) + encryptor.finalize()
+tag = encryptor.tag
+
+
+print (ciphertext)
+
+
+decryptor = cipher.decryptor()
+
+# Verify the integrity of the ciphertext and the authenticity of the sender
+try:
+    decryptor.authenticate_additional_data(tag)
+    print("Looks good")
+except Exception as e:
+    print("The ciphertext has been tampered with or the sender is not authentic.")
+
+# print(decryptor.update(ciphertext).decode())
+# Decrypt the message    
+# plaintext = decryptor.update(ciphertext) + decryptor.finalize(decryptor.tag)
+# # tag = decryptor.tag
+
+
+# print(plaintext) # b"This is a secret message."
