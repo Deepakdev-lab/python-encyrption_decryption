576215f28f0aed861e235170940cac0dc789eb0b
Deepakdev-lab <81613190+Deepakdev-lab@users.noreply.github.com>
2023-01-24 13:06:52 +0530

    Working Encryption and Decryption of a text using AES-GCM with Tag Validation
    Working Encryption and Decryption of a text using AES-GCM with Tag Validation

--- Encrypt_Decrypt_With_AES_GCM.py
+++ Encrypt_Decrypt_With_AES_GCM.py
@@ -2,36 +2,34 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
 
-# Generate a random 256-bit key (32 bytes)
-key = os.urandom(32)
+# Generate a random 128-bit key for AES-GCM
+key = os.urandom(16)
 
-# Generate a random 12-byte IV
-iv = os.urandom(12)
+# Data to be encrypted
+plaintext = b"hello world"
 
-# Create a new AES-GCM cipher
-cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
+# Initialize AES-GCM cipher
+cipher = Cipher(algorithms.AES(key), modes.GCM(b'\x00' * 12), backend=default_backend())
 encryptor = cipher.encryptor()
 
-# Encrypt the message
-actual_message = "This is a secret message."
-message = bytes(actual_message, 'utf-8')
+# Encrypt the data
+ciphertext = encryptor.update(plaintext) + encryptor.finalize()
 
-ciphertext = encryptor.update(message) + encryptor.finalize()
+# Get the authentication tag
 tag = encryptor.tag
 
-
-print (ciphertext)
-
-
+# Initialize AES-GCM cipher
 decryptor = cipher.decryptor()
+
+# Decrypt the data
+decrypted_plaintext = decryptor.update(ciphertext)
 
 # Verify the integrity of the ciphertext and the authenticity of the sender
 try:
-    decryptor.authenticate_additional_data(tag)
-    print("Looks good")
+    decrypted_plaintext += decryptor.finalize_with_tag(tag)
 except Exception as e:
     print("The ciphertext has been tampered with or the sender is not authentic.")
 
-# Decrypt the message    
-plaintext = decryptor.update(ciphertext) + decryptor.finalize(tag)
-print(plaintext) # b"This is a secret message."
+
+print (decrypted_plaintext.decode())
+
